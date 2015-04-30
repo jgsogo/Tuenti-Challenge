856fa59a95fad9894c2786ade435ff45ce26fb3f
Javier G. Sogo <jgsogo@gmail.com>
2015-04-30 17:44:22 +0200

    working on data

--- 6.Airscrews/main.cpp
+++ 6.Airscrews/main.cpp
@@ -2,58 +2,79 @@
 
 #include <iostream>
 #include <fstream>
+#include <stdint.h>
 #include <vector>
-#include <map>
-#include <algorithm>
 #include <sstream>
-#include <limits>
+#include <assert.h>
 
-#include "xz.h"  //! TODO: XZ embedded in project http://tukaani.org/xz/embedded.html
-#include <boost/multiprecision/cpp_int.hpp> // We need really big integers
-
-/* Tuenti Challenge #3: Favourite primes
-*
-*   Implementation on my own, really slow doing factorization :/
-*   We could use a third party library like http://sourceforge.net/projects/msieve/
-*   that implements GNFS (General Number Field Sieve algorithm).
+/* Tuenti Challenge #6: Airscrews
+*   - We have a MxN matrix (let's keep it generic, but being {M,N}<=5000)
 */
 
+#define MAX_SIZE ((uint16_t)5000)
 
-// Get N first prime numbers: will be called once, no need to optimize
-std::vector<std::size_t> compute_prime_numbers(const std::size_t& n) {
-    std::vector<std::size_t> primes;
-    if (n>=1) {
-        primes.push_back(2);
-        std::size_t prime_candidate = 3;
-        while(primes.size() < n) {
-            if (std::all_of(primes.begin(), primes.end(), [prime_candidate](const std::size_t& prime) { return prime_candidate % prime != 0;})) {
-                primes.push_back(prime_candidate);
+struct Data {
+    uint16_t* data;
+    long rows, cols;
+    uint16_t col_min, row_min, col_max, row_max;
+
+    Data() {
+        data = (uint16_t*)(malloc (sizeof(int) *MAX_SIZE*MAX_SIZE));
+        col_min = row_min = 0;
+        col_max = row_max = MAX_SIZE;
+        }
+    ~Data() {
+        free(data);
+        };
+    void set_limits(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1) {
+        col_min = x0; row_min = y0; col_max = x1; row_max = y1;
+        };
+    int parse_file(const std::string& file) {
+        std::ifstream datafile(file);
+        if (!datafile.is_open()) {
+            return -1;
+            }
+        std::string line;
+        std::getline(datafile, line);
+        std::stringstream stream(line);
+        stream >> rows >> cols;
+        for (auto row = 0; row < rows; ++row) {
+            std::getline(datafile, line);
+            if (row >= row_min && row <= row_max) {
+                std::stringstream stream(line);
+                std::size_t col = 0;
+                while (!stream.eof()) {
+                    stream >> data[row*cols + col++];
+                    }
                 }
-            prime_candidate += 2; // only check odd numbers.
+            else if (row > row_max) {
+                break;
+                }
             }
-        }
-    return primes;
-    }
-
-
-// Count factors for large number. Returns true if the rest is 1 (completely factorized).
-bool count_factors(const boost::multiprecision::cpp_int& number, const std::vector<std::size_t>& factors, std::map<std::size_t, std::size_t>& count) {
-    boost::multiprecision::cpp_int rest = number;
-    for (auto it = factors.rbegin(); it!=factors.rend(); ++it) {
-        while(0== boost::multiprecision::integer_modulus(rest, *it)) {
-            rest = rest/ *it;
-            count[*it] += 1;
+        };
+    uint16_t get_value(uint16_t row, uint16_t col) {
+        assert(row >= row_min); assert(row <= row_max);
+        assert(col >= col_min); assert(col <= col_max);
+        return data[row*cols + col];
+        };
+    long get_sum(uint16_t row0, uint16_t col0, uint16_t k) {
+        assert(row0 >=row_min);
+        assert(row0+k <= row_max);
+        assert(col0 >=col_min);
+        assert(col0+k <= col_max);
+        long sum = 0;
+        for (auto r = 0; r<k; ++r) {
+            for (auto c=0; c<k; ++c) {
+                sum += get_value(row0+r, col0+c);
+                }
             }
-        } 
-    return rest==1;
-    }
-
-// 
-void sum_counts(std::map<std::size_t, std::size_t>& accumulator, const std::map<std::size_t, std::size_t>& other) {
-    for (auto it = other.begin(); it!= other.end(); ++it) {
-        accumulator[it->first] += it->second;
+        return sum;
         }
-    }
+    };
+
+struct Case {
+    uint16_t col0, row0, col1, row1, k;
+    };
 
 // Main program: reads input file from argument
 int main (int argc, char *argv[]) {
@@ -67,59 +88,30 @@ int main (int argc, char *argv[]) {
     if (!file.is_open()) {
         return -1;
         }
-    
-    // Numbers.txt.xz file
-    std::string numbers_filename = "numbers.txt"; // TODO: Use xz-embedded to uncompress original file http://tukaani.org/
-    if (argc >= 3) {
-        numbers_filename = argv[2]; // Override numbers_file if this argument exists.
-        }
-    std::ifstream numbers_file(numbers_filename.c_str());
-    if (!numbers_file.is_open()) {
-        return -1;
-        }
-    std::vector<std::string> rock_content; // Really big numbers in the rock!.
-    std::string line;
-    std::size_t max_length = 0;
-    while (std::getline(numbers_file, line)) {
-        rock_content.push_back(line);
-        max_length = (std::max)(max_length, line.length());
-        }
-    std::map<std::size_t, std::map<std::size_t, std::size_t>> rock_precomputed;
 
-    // Get the 25 first prime numbers
-    std::vector<std::size_t> primes = compute_prime_numbers(25);
-
-    // Work over input file
-    std::size_t n_cases, start, end;
+    // Read cases
+    std::vector<Case> cases;
+    uint16_t n_cases, y0_min, x0_min, y1_max, x1_max, k;
+    y0_min = MAX_SIZE;
+    x0_min = MAX_SIZE;
+    y1_max = 0;
+    x1_max = 0;
     file >> n_cases;
-    const clock_t begin_time = clock();
-    while(file >> start >> end ) {
-        // Count factors for the rock_numbers indicated in the input file
-        std::map<std::size_t, std::size_t> counter;
-        auto it = rock_content.begin() + start;
-        auto it_end = rock_content.begin() + end;
-        for ( ; it!=it_end; ++it, ++start) {
-            auto inserted = rock_precomputed.insert(std::make_pair(start, std::map<std::size_t, std::size_t>()));
-            if (inserted.second) {
-                boost::multiprecision::cpp_int z(*it); // Create big number
-                auto check = count_factors(z, primes, inserted.first->second);
-                if (!check) {
-                    std::cout << "Number " << z << " is not divisible by factors!" << std::endl;
-                    }
-                }
-            sum_counts(counter, inserted.first->second);
-            }
-        // Get most common factors and print.
-        auto max_count = std::max_element(counter.begin(), counter.end(), [](const std::pair<std::size_t, std::size_t>& lhs, const std::pair<std::size_t, std::size_t>& rhs){ return lhs.second < rhs.second;});
-        std::cout << max_count->second;
-        for (auto it = counter.begin(); it!=counter.end(); ++it) {
-            if (it->second == max_count->second) {
-                std::cout << " " << it->first;
-                }
-            }
-        std::cout << std::endl;
+    for (auto i = 0; i<n_cases; ++i) {
+        auto it = cases.insert(cases.end(), Case());
+        file >> it->row0 >> it->col0 >> it->row1 >> it->col1 >> it->k;
+        y0_min = (std::min)(y0_min, it->row0);
+        x0_min = (std::min)(x0_min, it->col0);
+        y1_max = (std::max)(y1_max, it->row1);
+        x1_max = (std::max)(x1_max, it->col1);
         }
+    
+    // Read sheet.data
+    Data data;
+    data.set_limits(x0_min, y0_min, x1_max, y1_max);
+    data.parse_file("sheet.data");
 
-    //std::cout << float( clock () - begin_time ) /  CLOCKS_PER_SEC;
+    // And now start searching
+
     return 0;
 } 
