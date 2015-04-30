3b66443a99e1b8352c24a00330f65f5f945f8414
Javier G. Sogo <jgsogo@gmail.com>
2015-04-30 20:32:53 +0200

    a tope de log

--- 6.Airscrews/main.cpp
+++ 6.Airscrews/main.cpp
@@ -11,6 +11,10 @@
 
 /* Tuenti Challenge #6: Airscrews
 *   - We have a MxN matrix (let's keep it generic, but being {M,N}<=5000)
+*
+*   Optimizations:
+*       - Read just the data needed (it is passed in Data::set_limits) [TODO]
+*       - Precompute sums of a given size.
 */
 
 #define MAX_SIZE ((uint16_t)5000)
@@ -21,18 +25,24 @@ struct DataKey {
     bool operator<(const DataKey& other) const {
         return ((row < other.row) || (row==other.row && col<other.col) || (row==other.row && col==other.col && k<other.k));
         };
+    bool operator==(const DataKey& other) const {
+        return (row == other.row) && (col==other.col) && (k==other.k);
+        };
     };
 
 struct Data {
     uint16_t* data;
     long rows, cols; // Total size of data matrix
     uint16_t col_min, row_min, col_max, row_max; // Area of interest regarding our cases.
-    mutable std::map<DataKey, uint64_t> cached;
+    // Cache data
+    std::map<DataKey, uint64_t> cached;
+    std::size_t cache_size;
 
     Data() {
         data = (uint16_t*)(malloc (sizeof(int) *MAX_SIZE*MAX_SIZE));
         col_min = row_min = 0;
         col_max = row_max = MAX_SIZE;
+        cache_size = 0;
         }
     ~Data() {
         free(data);
@@ -52,31 +62,78 @@ struct Data {
         for (auto row = 0; row < rows; ++row) {
             // TODO: Advance to row_min
             std::getline(datafile, line);
-            if (row >= row_min && row <= row_max) {
+            //if (row >= row_min && row <= row_max) {
                 std::stringstream stream(line);
                 std::size_t col = 0;
                 // TODO: Read only columns of interest (¡optimization!)
                 while (!stream.eof()) {
                     stream >> data[row*cols + col++];
                     }
-                }
-            else if (row > row_max) {
-                break;
+            //    }
+            //else if (row > row_max) {
+            //    break;
+            //    }
+            }
+        };
+    void precompute_cache(const std::size_t& size = 5) {
+        assert(rows%size == 0);
+        assert(cols%size == 0);
+        cache_size = size;
+        cached.clear();        
+        for (auto r=0; r<rows/size; r+=size) {
+            for (auto c=0; c<cols/size; c+=size) {
+                uint64_t sum = 0;
+                for (auto i=0; i<size; ++i) {
+                    sum += this->get_value(r+i, c, c+size);
+                    }                
+                cached.insert(std::make_pair(DataKey(r, c, size), sum));
                 }
             }
         };
     uint16_t get_value(uint16_t row, uint16_t col) const {
-        assert(row >= row_min); assert(row <= row_max);
-        assert(col >= col_min); assert(col <= col_max);
         return data[row*cols + col];
         };
     uint64_t get_value(uint16_t row, uint16_t col_init, uint16_t col_end) const {
-        assert(row >= row_min); assert(row <= row_max);
-        assert(col_init >= col_min); assert(col_init <= col_max);
-        assert(col_end >= col_min); assert(col_end <= col_max);
         return std::accumulate( &data[row*cols + col_init], &data[row*cols + col_end], 0, std::plus<uint64_t>());
-        //return data[row*cols + col];
         };
+    uint64_t get_sum(uint16_t row0, uint16_t col0, uint16_t k) const {
+        auto r = k%cache_size;
+        auto n = k/cache_size;
+        auto init_row = (cache_size - row0%cache_size)%cache_size;
+        auto init_col = (cache_size - col0%cache_size)%cache_size;
+        uint64_t sum = 0;
+        std::cout << "row0 = " << row0 << ", col0 = " << col0 << ", k = " << k << std::endl;
+        // Rows not fitting with cache chunks (up)
+        std::cout << "\t for(" << row0 << "; " << row0+init_row << "; ++1) ==> Suma todas las columnas (arriba)" << std::endl;
+        for (auto ir = row0; ir<row0+init_row; ++ir) {
+            std::cout << "." << std::endl;
+            sum += get_value(ir, col0, col0+k);
+            }
+        std::cout << "\t for(" << row0+k-r+init_row << "; " << row0+k << "; ++1) ==> Suma todas las columnas (abajo)" << std::endl;
+        // Rows not fitting with cache chunks (down)
+        for (auto ir = row0+k-r+init_row; ir<row0+k; ++ir) {
+            std::cout << "." << std::endl;
+            sum += get_value(ir, col0, col0+k);
+            }
+        std::cout << "\t for(" << row0+init_row << "; " << row0+k-r+init_row << "; ++1) ==> Suma los trozos a derecha e izquierda." << std::endl;
+        // Cols not fitting with cache chunks (left + right)
+        for (auto ir = row0+init_row; ir < row0+k-r+init_row; ++ir) {
+            std::cout << "." << std::endl;
+            sum += get_value(ir, col0, col0+init_col);
+            sum += get_value(ir, col0+k-r+init_col, col0+k);
+            }
+        // Cache chunks
+        std::cout << "\t for(" << row0+init_row << "; " << row0+k << "; +=cache_size" << std::endl;
+        std::cout << "\t for(" << col0+init_col << "; " << col0+k << "; +=cache_size" << std::endl;
+        for (auto ir = row0+init_row; ir < row0+k-r+init_row; ir+=cache_size) {
+            for (auto ic = col0+init_col; ic<col0+k-r+init_col; ic+=cache_size) {
+                std::cout << "." << std::endl;
+                sum += cached.find(DataKey(ir, ic, cache_size))->second;
+                }
+            }
+        return sum;
+        };
+    /*
     uint64_t get_sum(uint16_t row0, uint16_t col0, uint16_t k) const {
         auto it = cached.insert(std::make_pair(DataKey(row0, col0, k), uint64_t() ));
         if (!it.second) {
@@ -97,11 +154,12 @@ struct Data {
         it.first->second = sum;
         return sum;
         }
+    */
     };
+    
 
 struct Case {
     uint16_t col0, row0, col1, row1, k;
-
     uint64_t compute(const Data& data);
     };
 
@@ -139,6 +197,7 @@ int main (int argc, char *argv[]) {
     Data data;
     data.set_limits(x0_min, y0_min, x1_max, y1_max);
     data.parse_file("sheet.data");
+    data.precompute_cache();
 
     // And now start searching
     int i = 1;
@@ -160,6 +219,7 @@ uint64_t Case::compute(const Data& data) {
             auto aspa1 = data.get_sum(row0+r, col0+c, k);
             auto aspa2 = data.get_sum(row0+r+k+1, col0+c+k+1, k);
             best_so_far = (std::max)(best_so_far, aspa1+aspa2);
+            throw std::exception();
             }
         }
     return best_so_far;
