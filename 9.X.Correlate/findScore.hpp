1ec94f817c140c24d8156214e7b48859e09859e0
Javier G. Sogo <jgsogo@gmail.com>
2015-05-01 22:33:43 +0200

    ganamos mucho yendo con una onda de tamaño constanste

--- 9.X.Correlate/findScore.hpp
+++ 9.X.Correlate/findScore.hpp
@@ -3,16 +3,21 @@
 
 #define THRESCORR 1e-30
 
-double crosscorr(const double* x, int xSize, double xSum, const double * y, int ySize, double yMean, double ySumCuadraticDiff) {
+struct WaveChunkData {
+    const double* data; 
+    int Size;
+    double Sum;  // Sum of all the elements: x1 + x2 + ... xn
+    double Sum2; // Sum of squares: x1^2 + x2^2 + ... xn^2
+    };
+
+//double crosscorr(const double* x, int xSize, double xSum, const double * y, int ySize, double yMean, double ySumCuadraticDiff) {
+double crosscorr(const WaveChunkData& x, const double * y, int ySize, double yMean, double ySumCuadraticDiff) {
     //std::cout << x[0] << "|" << xSize << " >>" << std::endl;
     //! Calculate the mean of the two series x[], y[]
-    double xMean = xSum/xSize;
+    double xMean = x.Sum/x.Size;
 
     //! Calculate the denominator (product of standard deviations)
-    double xSumCuadraticDiff = 0.0;
-    for (int i = 0; i < xSize; ++i) {
-        xSumCuadraticDiff += pow(x[i] - xMean, 2);
-        }
+    double xSumCuadraticDiff = x.Sum2 + x.Size*xMean*xMean - 2*xMean*x.Sum;
     
     double denom = sqrt(xSumCuadraticDiff * ySumCuadraticDiff);
     if (denom < THRESCORR){
@@ -22,15 +27,15 @@ double crosscorr(const double* x, int xSize, double xSum, const double * y, int 
     //! Calculate the correlation series
     double best_xcorr = 0.0;
 
-    for (int delay = 0; delay < (ySize - xSize + 1); ++delay) {
+    for (int delay = 0; delay < (ySize - x.Size + 1); ++delay) {
         double xySum = 0.0;
-        for (int i = 0; i < xSize; ++i) {
-            xySum += (x[i] - xMean) * (y[i + delay] - yMean);
+        for (int i = 0; i < x.Size; ++i) {
+            xySum += (x.data[i] - xMean) * (y[i + delay] - yMean);
             }
         best_xcorr = std::max(best_xcorr, xySum / denom);
         }
     //std::cout << " >>>> " << best_xcorr << " *\t" << xSize << " =\t" << best_xcorr*xSize << std::endl;
-    return best_xcorr*xSize;
+    return best_xcorr*x.Size;
     }
 
 double findScore(const double* wave, int waveSize, const double* pattern, int patternSize){
@@ -53,14 +58,22 @@ double findScore(const double* wave, int waveSize, const double* pattern, int pa
         }
     */
 
+    // It is better to go with a constant length wave and just update sum of its elements (specially for large chunks).
     int maxSubvectorLength = std::min(waveSize, patternSize);
-    for (int subvectorLength = minSubvectorLength; subvectorLength<=maxSubvectorLength; ++subvectorLength) {        
-        double xSum = std::accumulate(wave, wave+subvectorLength, 0.0);
+    for (int subvectorLength = minSubvectorLength; subvectorLength<=maxSubvectorLength; ++subvectorLength) {
+        // All these xcorr will be computed for the same chunk length
+        WaveChunkData xData;
+        xData.Size = subvectorLength;
+        xData.Sum = std::accumulate(wave, wave+subvectorLength, 0.0);
+        xData.Sum2 = std::inner_product(wave, wave+subvectorLength, wave, 0.0);
         for(int subvectorStart=0; subvectorStart <= waveSize - minSubvectorLength; ++subvectorStart) {
             if (subvectorLength <= waveSize - subvectorStart) {
-                double best_xcorr = crosscorr(&(wave[subvectorStart]), subvectorLength, xSum, pattern, patternSize, pMean, pSumCuadraticDiff);
+                xData.data = &(wave[subvectorStart]);
+                double best_xcorr = crosscorr(xData, pattern, patternSize, pMean, pSumCuadraticDiff);
                 score = std::max(score, best_xcorr);
-                xSum += wave[subvectorStart+subvectorLength] - wave[subvectorStart];
+                // Update values
+                xData.Sum += wave[subvectorStart+subvectorLength] - wave[subvectorStart];
+                xData.Sum2 += wave[subvectorStart+subvectorLength]*wave[subvectorStart+subvectorLength] - wave[subvectorStart]*wave[subvectorStart];
                 }
             }
         }
