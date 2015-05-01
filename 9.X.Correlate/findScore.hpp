ec04f7f9605a0ee340f66052659ac815a99bc8c0
Javier G. Sogo <jgsogo@gmail.com>
2015-05-01 22:45:40 +0200

    estudio si la media del trozo de onda es significativa

--- 9.X.Correlate/findScore.hpp
+++ 9.X.Correlate/findScore.hpp
@@ -10,9 +10,8 @@ struct WaveChunkData {
     double Sum2; // Sum of squares: x1^2 + x2^2 + ... xn^2
     };
 
-//double crosscorr(const double* x, int xSize, double xSum, const double * y, int ySize, double yMean, double ySumCuadraticDiff) {
 double crosscorr(const WaveChunkData& x, const double * y, int ySize, double yMean, double ySumCuadraticDiff) {
-    //std::cout << x[0] << "|" << xSize << " >>" << std::endl;
+    std::cout << x.data[0] << "|" << " >>";
     //! Calculate the mean of the two series x[], y[]
     double xMean = x.Sum/x.Size;
 
@@ -34,8 +33,8 @@ double crosscorr(const WaveChunkData& x, const double * y, int ySize, double yMe
             }
         best_xcorr = std::max(best_xcorr, xySum / denom);
         }
-    //std::cout << " >>>> " << best_xcorr << " *\t" << xSize << " =\t" << best_xcorr*xSize << std::endl;
-    return best_xcorr*x.Size;
+    std::cout << " >>>> " << best_xcorr << std::endl;
+    return best_xcorr;
     }
 
 double findScore(const double* wave, int waveSize, const double* pattern, int patternSize){
@@ -59,23 +58,29 @@ double findScore(const double* wave, int waveSize, const double* pattern, int pa
     */
 
     // It is better to go with a constant length wave and just update sum of its elements (specially for large chunks).
+    std::cout << "Wave media: " << std::accumulate(wave, wave+waveSize, 0.0) << std::endl;
     int maxSubvectorLength = std::min(waveSize, patternSize);
     for (int subvectorLength = minSubvectorLength; subvectorLength<=maxSubvectorLength; ++subvectorLength) {
+        std::cout << "Length: " << subvectorLength << std::endl;
         // All these xcorr will be computed for the same chunk length
         WaveChunkData xData;
         xData.Size = subvectorLength;
         xData.Sum = std::accumulate(wave, wave+subvectorLength, 0.0);
         xData.Sum2 = std::inner_product(wave, wave+subvectorLength, wave, 0.0);
+        double best_xcorr = 0.0;
         for(int subvectorStart=0; subvectorStart <= waveSize - minSubvectorLength; ++subvectorStart) {
             if (subvectorLength <= waveSize - subvectorStart) {
+                std::cout << "\t media: " << xData.Sum << "\t\t" << xData.Sum2 << std::endl;
                 xData.data = &(wave[subvectorStart]);
-                double best_xcorr = crosscorr(xData, pattern, patternSize, pMean, pSumCuadraticDiff);
-                score = std::max(score, best_xcorr);
-                // Update values
+                best_xcorr = std::max(best_xcorr, crosscorr(xData, pattern, patternSize, pMean, pSumCuadraticDiff)) ;
+                
+                // Update values of sum and sum^2
                 xData.Sum += wave[subvectorStart+subvectorLength] - wave[subvectorStart];
                 xData.Sum2 += wave[subvectorStart+subvectorLength]*wave[subvectorStart+subvectorLength] - wave[subvectorStart]*wave[subvectorStart];
                 }
             }
+        score = std::max(score, best_xcorr*subvectorLength);
+        std::cout << std::endl << std::endl;
         }
 
     return score;
